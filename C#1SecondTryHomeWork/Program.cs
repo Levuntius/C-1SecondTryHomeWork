fb274beeb23714d1f034542cbbc6b0f2cad27252
Leo <levgladishev@icloud.com>
2025-09-06 16:21:22 +0100

    delete everything

--- C#1SecondTryHomeWork/Program.cs
+++ C#1SecondTryHomeWork/Program.cs
@@ -4,12 +4,8 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Why does she never like what she ordered at a restaurant?");
-            Console.WriteLine("what did she order, and she always likes what I ordered?");
-            Console.WriteLine("And she starts eating from my plate. I tell her:");
-            Console.WriteLine("<<Order the same for yourself>>. She says: <<Why?");
-            Console.WriteLine("I just want to try>>. And she eats half of it.");
-            Console.WriteLine("// movie What Men Talk About //");
+         
+
         }
     }
 }
