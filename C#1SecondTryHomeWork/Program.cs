dee6d8832510a47a2c1332b337c2e97a92f2511d
Leo <levgladishev@icloud.com>
2025-09-07 15:18:14 +0100

    added encoding change to the code

--- C#1SecondTryHomeWork/Program.cs
+++ C#1SecondTryHomeWork/Program.cs
@@ -4,7 +4,8 @@
     {
         static void Main(string[] args)
         {
-
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            Console.InputEncoding = System.Text.Encoding.UTF8;
             string name;
             int age;
             float height;
