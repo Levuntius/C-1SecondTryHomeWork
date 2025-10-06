57b942cf670f6c8c8c3efaaeded6a11b41d5af61
Leo <levgladishev@icloud.com>
2025-10-06 07:39:22 +0100

    all data types and their values

--- C#1SecondTryHomeWork/Program.cs
+++ C#1SecondTryHomeWork/Program.cs
@@ -4,7 +4,21 @@
     {
         static void Main(string[] args)
         {
-         
+            Console.WriteLine("bool=: true, false");
+            Console.WriteLine("byte=: 0 to 255");
+            Console.WriteLine("sbyte=: -128 to 127");
+            Console.WriteLine("short=: -32,768 to 32,767");
+            Console.WriteLine("ushort=: 0 to 65,535");
+            Console.WriteLine("int=: -2,147,483,648 to 2,147,483,647");
+            Console.WriteLine("uint=: 0 to 4,294,967,295");
+            Console.WriteLine("long=: -9,223,372,036,854,775,808 to 9,223,372,036,854,775,807");
+            Console.WriteLine("ulong=: 0 to 18,446,744,073,709,551,615");
+            Console.WriteLine("float=: decimal number (6-9 digits)");
+            Console.WriteLine("double=: decimal number (15-17 digits)");
+            Console.WriteLine("decimal=: decimal number (28-29 digits)");
+            Console.WriteLine("char=: a single 16-bit Unicode character");
+            Console.WriteLine("string=: a sequence of characters");
+            Console.WriteLine("object=: any data type");
 
         }
     }
