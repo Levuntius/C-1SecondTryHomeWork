c9b2d4681d80c35bab79dab8f22ac41f95f7bf29
Leo <levgladishev@icloud.com>
2025-10-26 16:23:31 +0000

    change rectange numbers

--- C#1SecondTryHomeWork/Program.cs
+++ C#1SecondTryHomeWork/Program.cs
@@ -5,8 +5,8 @@
         static void Main(string[] args)
         {
             //5.a//
-            double Length_of_a_rectangle = 10.5;
-            double Width_of_a_rectangle = 5.5;
+            double Length_of_a_rectangle = 10.0;
+            double Width_of_a_rectangle = 5.0;
             double Area_of_a_rectangle = Length_of_a_rectangle * Width_of_a_rectangle;
             Console.WriteLine("Area of a rectangle: " + Area_of_a_rectangle);
            
