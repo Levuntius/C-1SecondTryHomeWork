2829a944720db2f0e2798aa49649efe8ba9a12aa
Leo <levgladishev@icloud.com>
2025-10-26 16:09:18 +0000

    5.a

--- C#1SecondTryHomeWork/Program.cs
+++ C#1SecondTryHomeWork/Program.cs
@@ -4,7 +4,13 @@
     {
         static void Main(string[] args)
         {
-         
+            //5.a//
+            double Length_of_a_rectangle = 10.5;
+            double Width_of_a_rectangle = 5.5;
+            double Area_of_a_rectangle = Length_of_a_rectangle * Width_of_a_rectangle;
+            Console.WriteLine("Area of a rectangle: " + Area_of_a_rectangle);
+
+
 
         }
     }
