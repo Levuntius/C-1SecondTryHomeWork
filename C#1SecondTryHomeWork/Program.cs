c2521f5cb6ce26e1ac17a016e01d67651147cd4d
Leo <levgladishev@icloud.com>
2025-12-07 14:11:00 +0000

    done

--- C#1SecondTryHomeWork/Program.cs
+++ C#1SecondTryHomeWork/Program.cs
@@ -4,8 +4,37 @@
     {
         static void Main(string[] args)
         {
-         
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            Console.InputEncoding = System.Text.Encoding.UTF8;
+            try
+            {
+                Console.Write("Введите число a: ");
+                int a = int.Parse(Console.ReadLine());
 
-        }
-    }
+                Console.Write("Введите число b: ");
+                int b = int.Parse(Console.ReadLine());
+
+                int start = Math.Min(a, b);
+                int end = Math.Max(a, b);
+
+                int sum = 0;
+
+                for (int i = start; i <= end; i++)
+                {
+                    if (i % 2 != 0)
+                    {
+                        sum += i * i;
+                    }
+                }
+                Console.WriteLine($"Сумма квадратов нечётных чисел от {a} до {b} = {sum}");
+
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Ошибка: Введено некорректное число.");
+            }
+
+        }   
+     }
+
 }
