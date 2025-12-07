07669780dd22a560b77578394836646e21ae4ea9
Leo <levgladishev@icloud.com>
2025-12-07 17:05:33 +0000

    done

--- C#1SecondTryHomeWork/Program.cs
+++ C#1SecondTryHomeWork/Program.cs
@@ -4,8 +4,49 @@
     {
         static void Main(string[] args)
         {
-         
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            Console.InputEncoding = System.Text.Encoding.UTF8;
+            string password = "12345";
+            int attempts = 5;
+
+            try
+            {
+
+                while (attempts > 0)
+                {
+                    Console.Write("Введите пароль: ");
+                    string input = Console.ReadLine();
+
+                    if (input == password)
+                    {
+                        Console.WriteLine("Добро пожаловать");
+                        return; // завершаем программу
+                    }
+                    else
+                    {
+                        attempts--;
+                        if (attempts > 0)
+                        {
+                            Console.WriteLine($"Неверный пароль. Осталось попыток: {attempts}");
+                        }
+                        else
+                        {
+                            Console.WriteLine("Попытки закончились");
+                        }
+                    }
+                }
+
+
+            }
+
+            catch (FormatException)
+            {
+                Console.WriteLine($"То что вы ввели не может быть паролем");
+            }
 
         }
+
     }
+
 }
+
