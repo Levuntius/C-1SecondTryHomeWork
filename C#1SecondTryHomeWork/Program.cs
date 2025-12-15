bbc9c0aff98ff3fe72dc16763aa2680ff9cfcf8e
Leo <levgladishev@icloud.com>
2025-12-15 10:39:54 +0000

    done?

--- C#1SecondTryHomeWork/Program.cs
+++ C#1SecondTryHomeWork/Program.cs
@@ -7,19 +7,22 @@
             Console.OutputEncoding = System.Text.Encoding.UTF8;
             Console.InputEncoding = System.Text.Encoding.UTF8;
 
-            int a;
-            int b;
-            int c;
-            int d;
             bool positive;
+            int fourDigitNum;
             int numOfEvenDigits = 0;
 
             Console.WriteLine("введите четерех значное число");
-            try
+            while (true)
             {
-                int fourDigitNum = int.Parse(Console.ReadLine());
+                if (!int.TryParse(Console.ReadLine(), out fourDigitNum))
+                {
+                    Console.WriteLine("Ошибка ввода");
+                    continue;
+                }
+                break;
+            }
 
-                if (fourDigitNum >= 0)
+            if (fourDigitNum >= 0)
                 {
                     positive = true;
                 }
@@ -28,44 +31,32 @@
                     positive = false;
                 }
 
-                if (fourDigitNum > 1000 || fourDigitNum < 9999 && fourDigitNum < -1000 || fourDigitNum > -9999)
+                if (fourDigitNum >= 1000 || fourDigitNum <= 9999 && fourDigitNum <= -1000 || fourDigitNum >= -9999)
                 {
 
                     if (positive)
                     {
-                        a = fourDigitNum / 1000;
-                        b = (fourDigitNum / 100) % 10;
-                        c = (fourDigitNum / 10) % 10;
-                        d = fourDigitNum % 10;
+                        for(int i = 1000; i != 0; i/= 10)
+                        
+                            if (fourDigitNum / i % 10 % 2 == 0);
+                            {
+                               numOfEvenDigits++;
+                            }
+
                     }
                     else
                     {
-                        a = (fourDigitNum / 1000) * -1;
-                        b = ((fourDigitNum / 100) % 10) * -1;
-                        c = ((fourDigitNum / 10) % 10) * -1;
-                        d = (fourDigitNum % 10) * -1;
-                    }
-                    if (a % 2 == 0) 
-                    {
-                        numOfEvenDigits ++;
-                    }
-                    if (b % 2 == 0)
-                    {
-                        numOfEvenDigits ++;
-                        }
-                    if (c % 2 == 0)
-                    {
-                        numOfEvenDigits ++;
-                    }
-                    if (d % 2 == 0)
-                    {
-                        numOfEvenDigits++;
-                    } 
+                        for (int i = 1000; i != 0; i /= 10)
+
+                            if (-1 * fourDigitNum / i % 10 % 2 == 0);
+                            {
+                               numOfEvenDigits++;
+                            }
+                }
+                    
+                    
                         Console.WriteLine($"Количество четных цифр: {numOfEvenDigits}");
-                        Console.WriteLine($"Первая цифра: {a}");
-                        Console.WriteLine($"Вторая цифра: {b}");
-                        Console.WriteLine($"Третяя цифра: {c}");
-                        Console.WriteLine($"Четвертая цифра: {d}");
+                        
                 }
 
                 
@@ -74,16 +65,10 @@
                 { 
                    Console.WriteLine("Не четерех значное число");
                 }
-            }
-            catch (FormatException)
-            {
-                Console.WriteLine("Непрввильный формат");
-            }
+            
+
            
-            catch (DivideByZeroException)
-            {
-                Console.WriteLine($"Деление на ноль");
-            }
+           
         }
     }
 }
