e93af599cfc77d76ddb2421de116ed5e605865b9
Leo <levgladishev@icloud.com>
2026-01-23 09:06:03 +0000

    done?

--- C#1SecondTryHomeWork/Program.cs
+++ C#1SecondTryHomeWork/Program.cs
@@ -4,8 +4,52 @@
     {
         static void Main(string[] args)
         {
-         
+            int[] arr = new int[20];
+            for (int i = 0; i < arr.Length; i++)
+            {
+                array[i] = Random.Shared.Next(51);
 
-        }
+            }
+
+            bool swapped = true;
+            int start = 0;
+            int end = arr.Length - 1;
+
+            while (swapped)
+            {
+                swapped = false;
+
+                // Проход слева направо
+                for (int i = start; i < end; i++)
+                {
+                    if (arr[i] > arr[i + 1])
+                    {
+                        int temp = arr[i];
+                        arr[i] = arr[i + 1];
+                        arr[i + 1] = temp;
+                        swapped = true;
+                    }
+                }
+
+                if (!swapped)
+                    break;
+
+                swapped = false;
+                end--;
+
+                // Проход справа налево
+                for (int i = end; i > start; i--)
+                {
+                    if (arr[i] < arr[i - 1])
+                    {
+                        int temp = arr[i];
+                        arr[i] = arr[i - 1];
+                        arr[i - 1] = temp;
+                        swapped = true;
+                    }
+                }
+
+                start++;
+            }
     }
 }
