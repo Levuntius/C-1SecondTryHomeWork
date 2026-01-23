4778508a6dbcfe5174587ca72a2d6e50607f8657
Leo <levgladishev@icloud.com>
2026-01-23 09:41:56 +0000

    5

--- C#1SecondTryHomeWork/Program.cs
+++ C#1SecondTryHomeWork/Program.cs
@@ -1,84 +1,91 @@
-﻿namespace C_1SecondTryHomeWork
+﻿using System.Text;
+
+namespace C_1SecondTryHomeWork
 {
     internal class Program
     {
         static void Main(string[] args)
         {
+            Console.OutputEncoding = Encoding.UTF8;
+            Console.InputEncoding = Encoding.UTF8;
 
-              int[] arr = new int[20];
-              for (int i = 0; i < arr.Length; i++)
-              {
-                  arr[i] = Random.Shared.Next(51);
-
-              }
-
-              Console.WriteLine(string.Join(", ", arr));
-
-              bool swapped = true;
-              int start = 0;
-              int end = arr.Length - 1;
-
-              while (swapped)
-              {
-                  swapped = false;
-
-                  // Проход слева направо
-                  for (int i = start; i < end; i++)
-                  {
-                      if (arr[i] > arr[i + 1])
-                      {
-                          int temp = arr[i];
-                          arr[i] = arr[i + 1];
-                          arr[i + 1] = temp;
-                          swapped = true;
-                      }
-                  }
-
-                  if (!swapped)
-                      break;
-
-                  swapped = false;
-                  end--;
-
-                  // Проход справа налево
-                  for (int i = end; i > start; i--)
-                  {
-                      if (arr[i] < arr[i - 1])
-                      {
-                          int temp = arr[i];
-                          arr[i] = arr[i - 1];
-                          arr[i - 1] = temp;
-                          swapped = true;
-                      }
-                  }
-
-                  start++;
-              }
-              Console.WriteLine(string.Join(", ", arr));
-
-            int[] arr = new int[20];
+            /*int[] arr = new int[20];
             for (int i = 0; i < arr.Length; i++)
             {
                 arr[i] = Random.Shared.Next(51);
 
             }
 
-            for (int i = 1; i < arr.Length; i++)
+            Console.WriteLine(string.Join(", ", arr));
+
+            bool swapped = true;
+            int start = 0;
+            int end = arr.Length - 1;
+
+            while (swapped)
+            {
+                swapped = false;
+
+                // Проход слева направо
+                for (int i = start; i < end; i++)
+                {
+                    if (arr[i] > arr[i + 1])
+                    {
+                        int temp = arr[i];
+                        arr[i] = arr[i + 1];
+                        arr[i + 1] = temp;
+                        swapped = true;
+                    }
+                }
+
+                if (!swapped)
+                    break;
+
+                swapped = false;
+                end--;
+
+                // Проход справа налево
+                for (int i = end; i > start; i--)
+                {
+                    if (arr[i] < arr[i - 1])
+                    {
+                        int temp = arr[i];
+                        arr[i] = arr[i - 1];
+                        arr[i - 1] = temp;
+                        swapped = true;
+                    }
+                }
+
+                start++;
+              }
+            Console.WriteLine(string.Join(", ", arr));*/
+
+
+            int[] arr2 = new int[20];
+            for (int i = 0; i < arr2.Length; i++)
+            {
+                arr2[i] = Random.Shared.Next(51);
+
+            }
+
+            Console.WriteLine(string.Join(", ", arr2));
+
+            for (int i = 1; i < arr2.Length; i++)
               {
-                  int key = arr[i];   // элемент, который вставляем
+                  int key = arr2[i];
                   int j = i - 1;
 
                   // Сдвигаем элементы вправо
-                  while (j >= 0 && arr[j] > key)
+                  while (j >= 0 && arr2[j] > key)
                   {
-                      arr[j + 1] = arr[j];
+                      arr2[j + 1] = arr2[j];
                       j--;
                   }
 
                   // Вставляем элемент
-                  arr[j + 1] = key;
+                  arr2[j + 1] = key;
               }
-              Console.WriteLine(string.Join(", ", arr));
+              Console.WriteLine(string.Join(", ", arr2));
 
 
         }
