930f7388b06deab44105b375dcf3cb8be57e313b
Leo <levgladishev@icloud.com>
2026-01-30 09:28:04 +0000

    addRandom.Shared.Next()

--- C#1SecondTryHomeWork/Program.cs
+++ C#1SecondTryHomeWork/Program.cs
@@ -6,16 +6,16 @@
         {
             Console.OutputEncoding = System.Text.Encoding.UTF8;
             Console.InputEncoding = System.Text.Encoding.UTF8;
-            Random rnd = new Random();
+
             int rows = 3, cols = 5;
             int[,] matrix = new int[rows, cols];
 
-            // заполнение
+            
             for (int i = 0; i < rows; i++)
             {
                 for (int j = 0; j < cols; j++)
                 {
-                    matrix[i, j] = rnd.Next(10, 21);
+                    matrix[i, j] = Random.Shared.Next(10, 21);
                     Console.Write(matrix[i, j] + "\t");
                 }
                 Console.WriteLine();
@@ -24,11 +24,11 @@
             int sum = 0;
             for (int j = 0; j < cols; j++)
             {
-                sum += matrix[1, j]; // 2 строка
-                sum += matrix[2, j]; // 3 строка
+                sum += matrix[1, j];
+                sum += matrix[2, j];
             }
 
-            Console.WriteLine("Сумма 2 и 3 строки: " + sum);
+            Console.WriteLine("Сумма 2 и 3 строка: " + sum);
         }
     }
 }
