5a3756356b7525d25a587234bd99266c2f2ec684
Leo <levgladishev@icloud.com>
2026-01-30 09:23:50 +0000

    1done

--- C#1SecondTryHomeWork/Program.cs
+++ C#1SecondTryHomeWork/Program.cs
@@ -1,11 +1,32 @@
 ﻿namespace C_1SecondTryHomeWork
 {
-    internal class Program
+    class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-         
+            Random rnd = new Random();
+            int rows = 3, cols = 5;
+            int[,] matrix = new int[rows, cols];
 
+            // заполнение
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < cols; j++)
+                {
+                    matrix[i, j] = rnd.Next(10, 21);
+                    Console.Write(matrix[i, j] + "\t");
+                }
+                Console.WriteLine();
+            }
+
+            int sum = 0;
+            for (int j = 0; j < cols; j++)
+            {
+                sum += matrix[1, j]; // 2 строка
+                sum += matrix[2, j]; // 3 строка
+            }
+
+            Console.WriteLine("Сумма 2 и 3 строки: " + sum);
         }
     }
 }
