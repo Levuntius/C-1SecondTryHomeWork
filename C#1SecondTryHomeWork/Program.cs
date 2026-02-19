1f5f39f8a1b4331825b7d7d9c8c2241047d8be9c
Leo <levgladishev@icloud.com>
2026-02-19 18:40:15 +0000

    do some job

--- C#1SecondTryHomeWork/Program.cs
+++ C#1SecondTryHomeWork/Program.cs
@@ -1,10 +1,53 @@
-﻿namespace C_1SecondTryHomeWork
+﻿using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace C_1SecondTryHomeWork
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-         
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            Console.InputEncoding = System.Text.Encoding.UTF8;
+
+            double balance = 26122019;
+            int number;
+
+            ShowWelcome();
+
+            while (true)
+            {
+                Console.Write("Введите число (1-4): ");
+                string input = Console.ReadLine();
+
+                if (int.TryParse(input, out number) && number >= 1 && number <= 4)
+                {
+                    Console.WriteLine("Вы выбрали: " + number);
+                    break; // выход из цикла
+                }
+                else
+                {
+                    Console.WriteLine("Ошибка! Введите число от 1 до 4.");
+                }
+
+            }
+
+
+
+
+
+
+
+
+
+            static void ShowWelcome()
+            {
+                Console.WriteLine("Здравствуйте напишите цифру для выбора операции");
+                Console.WriteLine("1. Проверить баланс");
+                Console.WriteLine("2. Пополнить счёт");
+                Console.WriteLine("3. Снять деньги");
+                Console.WriteLine("4. Посмотреть историю операций");
+            }
+
 
         }
     }
