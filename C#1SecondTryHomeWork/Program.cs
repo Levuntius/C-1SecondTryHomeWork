cf012f8a8623b2a3e4516cebaa9002abf22409b5
levglad <levgladishev@icloud.com>
2026-05-08 10:38:13 +0100

    k

--- C#1SecondTryHomeWork/Program.cs
+++ C#1SecondTryHomeWork/Program.cs
@@ -1,4 +1,6 @@
-﻿using System.Text;
+﻿using System;
+using System.Text;
+
 namespace C_1SecondTryHomeWork
 {
     internal class Program
@@ -7,17 +9,17 @@ namespace C_1SecondTryHomeWork
         {
             Console.OutputEncoding = Encoding.UTF8;
             Console.InputEncoding = Encoding.UTF8;
-            // КАРТЫ
+
             (char letter, int position)[] cards =
             {
-            ('A', 0), ('A', 1),
-            ('B', 2), ('B', 3),
-            ('C', 4), ('C', 5),
-            ('D', 6), ('D', 7),
-            ('E', 8), ('E', 9),
-            ('F', 10), ('F', 11),
-            ('G', 12), ('G', 13),
-            ('H', 14), ('H', 15)
+                ('A', 0), ('A', 1),
+                ('B', 2), ('B', 3),
+                ('C', 4), ('C', 5),
+                ('D', 6), ('D', 7),
+                ('E', 8), ('E', 9),
+                ('F', 10), ('F', 11),
+                ('G', 12), ('G', 13),
+                ('H', 14), ('H', 15)
             };
 
             Random random = new Random();
@@ -32,74 +34,75 @@ namespace C_1SecondTryHomeWork
                 cards[index2] = temp;
             }
 
-            // ПЕРВАЯ КАРТА 
-
-            int firstIndex;
-
             while (true)
             {
-                Console.Write("Выберите первую карту (1-16): ");
+                int firstIndex;
 
-                string input = Console.ReadLine();
-
-                bool success = int.TryParse(input, out firstIndex);
-
-                if (success && firstIndex >= 1 && firstIndex <= 16)
+                while (true)
                 {
-                    break;
+                    Console.Write("Первая карта 1-16 или 0 для выхода: ");
+                    string input = Console.ReadLine();
+
+                    if (int.TryParse(input, out firstIndex))
+                    {
+                        if (firstIndex == 0)
+                            return;
+
+                        if (firstIndex >= 1 && firstIndex <= 16)
+                            break;
+                    }
+
+                    Console.WriteLine("Ошыбка! Введите число 1-16");
                 }
 
-                Console.WriteLine("Ошыбка! Нужно ввести число от 1 до 16.");
-            }
+                firstIndex--;
 
-            firstIndex--;
+                Console.WriteLine($"Первая карта: {cards[firstIndex].letter}");
 
-            Console.WriteLine($"Первая карта: {cards[firstIndex].letter}");
+                int secondIndex;
 
-            // ВТОРАЯ КАРТА 
-
-            int secondIndex;
-
-            while (true)
-            {
-                Console.Write("Выберите вторую карту (1-16): ");
-
-                string input = Console.ReadLine();
-
-                bool success = int.TryParse(input, out secondIndex);
-
-                if (!success || secondIndex < 1 || secondIndex > 16)
+                while (true)
                 {
-                    Console.WriteLine("Ошибка! Нужно ввести число от 1 до 16");
-                    continue;
+                    Console.Write("Вторая карта 1-16: ");
+                    string input = Console.ReadLine();
+
+                    if (int.TryParse(input, out secondIndex))
+                    {
+                        if (secondIndex >= 1 && secondIndex <= 16)
+                        {
+                            secondIndex--;
+
+                            if (secondIndex == firstIndex)
+                            {
+                                Console.WriteLine("Нельзя выбрать ту же карту");
+                                continue;
+                            }
+
+                            break;
+                        }
+                    }
+
+                    Console.WriteLine("Ошыбка! Введите число 1-16");
                 }
 
-                secondIndex--;
+                Console.WriteLine($"Вторая карта: {cards[secondIndex].letter}");
 
-                if (secondIndex == firstIndex)
+                if (cards[firstIndex].letter == cards[secondIndex].letter)
                 {
-                    Console.WriteLine("Нельзя выбирать одну и ту же карту");
-                    continue;
+                    Console.WriteLine("Пара найдена");
+                }
+                else
+                {
+                    Console.WriteLine("Это не пара");
                 }
 
-                break;
+                Console.WriteLine();
+                Console.WriteLine("Нажмите Enter для следующего хода");
+                Console.ReadLine();
+                Console.Clear();
             }
-
-            Console.WriteLine($"Вторая карта: {cards[secondIndex].letter}");
-
-            // ПРОВЕРКА ПАРЫ 
-
-            if (cards[firstIndex].letter == cards[secondIndex].letter)
-            {
-                Console.WriteLine("Пара найдена");
-            }
-            else
-            {
-                Console.WriteLine("Это не пара");
-            }
-            Console.ReadKey();
         }
     }
 }
-    
+
 
