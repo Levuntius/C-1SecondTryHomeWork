16c31a90d7e9a4fa43ab746744b362dc75361296
levglad <levgladishev@icloud.com>
2026-05-08 10:29:16 +0100

    finaly

--- C#1SecondTryHomeWork/Program.cs
+++ C#1SecondTryHomeWork/Program.cs
@@ -4,6 +4,8 @@
     {
         static void Main()
         {
+            Console.OutputEncoding = Encoding.UTF8;
+            Console.InputEncoding = Encoding.UTF8;
             // КАРТЫ
             (char letter, int position)[] cards =
         {
