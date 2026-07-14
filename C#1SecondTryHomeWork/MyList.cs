0a46b1ebf1fd86e0b68689a1e9d59bce25d5eae5
levglad <levgladishev@icloud.com>
2026-07-14 09:35:01 +0100

    п

---
+++ C#1SecondTryHomeWork/MyList.cs
@@ -0,0 +1,106 @@
+﻿namespace C_1SecondTryHomeWork
+{
+    public class MyList
+    {
+        private string[] _items;
+        private int _count;
+
+        // Конструктор
+        public MyList()
+        {
+            _items = new string[5];
+            _count = 0;
+        }
+
+        public int Count => _count;
+
+        public int Capacity => _items.Length;
+
+        // Индексатор
+        public string this[int index]
+        {
+            get
+            {
+                if (index < 0 || index >= _count)
+                    throw new IndexOutOfRangeException("Неверный индекс");
+
+                return _items[index];
+            }
+        }
+
+        // Метод добавления элемента
+        public bool Add(string item)
+        {
+            if (_count >= _items.Length)
+            {
+                Console.WriteLine("Ошибка. Недостаточно выделенной памяти");
+                return false;
+            }
+
+            _items[_count] = item;
+            _count++;
+
+            Console.WriteLine("Элемент добавлен");
+            return true;
+        }
+
+        // Метод удаления элемента
+        public bool RemoveAt(int index)
+        {
+            if (index < 0 || index >= _count)
+            {
+                Console.WriteLine("Ошибка. Неверный индекс");
+                return false;
+            }
+
+            for (int i = index; i < _count - 1; i++)
+            {
+                _items[i] = _items[i + 1];
+            }
+
+            _items[_count - 1] = null;
+            _count--;
+
+            Console.WriteLine("Элемент удален");
+            return true;
+        }
+
+        // Изменение выделенной памяти
+        public void ChangeCapacity(int newCapacity)
+        {
+            if (newCapacity < _count)
+            {
+                Console.WriteLine("Ошибка. Новая память меньше количества элементов");
+                return;
+            }
+
+            string[] newItems = new string[newCapacity];
+
+            for (int i = 0; i < _count; i++)
+            {
+                newItems[i] = _items[i];
+            }
+
+            _items = newItems;
+
+            Console.WriteLine($"Выделенная память изменена на {newCapacity} элементов");
+        }
+
+        // Метод вывода элементов
+        public void DisplayItems()
+        {
+            if (_count == 0)
+            {
+                Console.WriteLine("Список пуст");
+                return;
+            }
+
+            Console.WriteLine("Элементы списка");
+
+            for (int i = 0; i < _count; i++)
+            {
+                Console.WriteLine($"[{i}] - {_items[i]}");
+            }
+        }
+    }
+}
--- C#1SecondTryHomeWork/Program.cs
+++ C#1SecondTryHomeWork/Program.cs
@@ -1,12 +1,97 @@
-﻿namespace C_1SecondTryHomeWork
+﻿using System;
+using System.Text;
+
+namespace C_1SecondTryHomeWork
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-           
-                
-         
+            Console.OutputEncoding = Encoding.UTF8;
+            Console.InputEncoding = Encoding.UTF8;
+
+            MyList myList = new MyList();
+            bool running = true;
+
+            while (running)
+            {
+                Console.WriteLine("Напишите:");
+                Console.WriteLine("1 если хотите добавить новый элемент в список");
+                Console.WriteLine("2 если хотите удалить элемент из списка");
+                Console.WriteLine("3 если хотите показать элементы списка");
+                Console.WriteLine("4 если хотите узнать, сколько памяти выделено");
+                Console.WriteLine("5 если хотите изменить выделенную память");
+                Console.WriteLine("0 если хотите выйти");
+
+                string choice = Console.ReadLine();
+
+                switch (choice)
+                {
+                    case "1":
+                        Console.Write("Введите строку для добавления: ");
+                        string textToAdd = Console.ReadLine();
+                        myList.Add(textToAdd);
+                        break;
+
+                    case "2":
+                        Console.Write("Введите индекс элемента для удаления: ");
+
+                        if (int.TryParse(Console.ReadLine(), out int indexToRemove))
+                        {
+                            myList.RemoveAt(indexToRemove);
+                        }
+                        else
+                        {
+                            Console.WriteLine("Ошибка: Введите корректное число.");
+                        }
+
+                        break;
+
+                    case "3":
+                        myList.DisplayItems();
+                        Console.WriteLine($"Всего элементов в списке: {myList.Count}");
+                        break;
+
+                    case "4":
+                        Console.WriteLine($"Выделено памяти под {myList.Capacity} элементов.");
+                        break;
+
+                    case "5":
+                        bool sizeUpdated = false;
+
+                        while (!sizeUpdated)
+                        {
+                            Console.Write("Введите новый размер памяти: ");
+
+                            if (int.TryParse(Console.ReadLine(), out int newSize))
+                            {
+                                if (newSize < myList.Count)
+                                {
+                                    Console.WriteLine($"Ошибка. Новый размер ({newSize}) меньше количества элементов ({myList.Count}).");
+                                }
+                                else
+                                {
+                                    myList.ChangeCapacity(newSize);
+                                    sizeUpdated = true;
+                                }
+                            }
+                            else
+                            {
+                                Console.WriteLine("Ошибка. Введите корректное число.");
+                            }
+                        }
+
+                        break;
+
+                    case "0":
+                        running = false;
+                        break;
+
+                    default:
+                        Console.WriteLine("Неверный ввод. Выберите пункт от 0 до 5.");
+                        break;
+                }
+            }
         }
     }
 }
