1b95b3aa18118f27f07c8a5a4f1bb8edd5b36903
levglad <levgladishev@icloud.com>
2026-07-21 00:57:31 +0100

    done

---
+++ C#1SecondTryHomeWork/MyStack.cs
@@ -0,0 +1,69 @@
+﻿namespace C_1SecondTryHomeWork
+
+{
+    public class MyStack
+    {
+        private int[] items;
+        private int count;
+
+        public MyStack()
+        {
+            items = new int[5];
+            count = 0;
+        }
+
+        public void Push(int value)
+        {
+            if (count == items.Length)
+            {
+                int[] newArray = new int[items.Length * 2+1];
+                for (int i = 0; i < items.Length; i++)
+                {
+                    newArray[i] = items[i];
+                }
+                items = newArray;
+            }
+
+            items[count] = value;
+            count++;
+        }
+
+        public int Pop()
+        {
+            if (count == 0)
+            {
+                throw new InvalidOperationException("Стек пуст");
+                return 0;
+            }
+
+            count--;
+            int val = items[count];
+            items[count] = 0;
+
+            return val;
+        }
+
+        public int Peek()
+        {
+            if (count == 0)
+            {
+                throw new InvalidOperationException("Стек пуст!");
+                return 0;
+            }
+
+            return items[count - 1];
+        }
+
+        public void Clear()
+        {
+            items = new int[4];
+            count = 0;
+        }
+
+        public int GetCount()
+        {
+            return count;
+        }
+    }
+}
+
--- C#1SecondTryHomeWork/Program.cs
+++ C#1SecondTryHomeWork/Program.cs
@@ -4,9 +4,28 @@
     {
         static void Main(string[] args)
         {
-           
-                
-         
+            MyStack stack = new MyStack();
+
+            stack.Push(10);
+            stack.Push(20);
+            stack.Push(30);
+
+            Console.WriteLine("Сейчас сверху: " + stack.Peek());
+
+            Console.WriteLine("Pop: " + stack.Pop());
+            Console.WriteLine("Pop: " + stack.Pop());
+
+            Console.WriteLine("Сейчас сверху: " + stack.Peek());
+
+            stack.Clear();
+
+            Console.WriteLine("После очистки Count = " + stack.GetCount());
+
+            Console.WriteLine("Pop: " + stack.Pop());
+
+            Console.ReadLine();
+
+
         }
     }
 }
