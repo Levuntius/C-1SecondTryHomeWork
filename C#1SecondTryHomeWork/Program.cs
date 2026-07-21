81df6d3f226a7802245816a8b8d028a1e0521a3b
levglad <levgladishev@icloud.com>
2026-07-21 01:02:10 +0100

    а

--- C#1SecondTryHomeWork/Program.cs
+++ C#1SecondTryHomeWork/Program.cs
@@ -1,9 +1,14 @@
-﻿namespace C_1SecondTryHomeWork
+﻿using System.Text;
+
+namespace C_1SecondTryHomeWork
 {
     internal class Program
     {
         static void Main(string[] args)
         {
+            Console.OutputEncoding = Encoding.UTF8;
+            Console.InputEncoding = Encoding.UTF8;
+
             MyStack stack = new MyStack();
 
             stack.Push(10);
