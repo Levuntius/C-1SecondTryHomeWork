47c9e7681d68bd53a73f6b496d34116cc2049173
levglad <levgladishev@icloud.com>
2026-09-15 07:22:30 +0100

    в

---
+++ C#1SecondTryHomeWork/Item.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_1SecondTryHomeWork
+{
+    class Item
+    {
+        public int Id;
+        public string Name;
+
+        public override int GetHashCode()
+        {
+            // Плохой хеш — всегда одинаковый
+            return 1;
+        }
+
+        public override bool Equals(object obj)
+        {
+            return obj is Item other &&
+                   Id == other.Id &&
+                   Name == other.Name;
+        }
+    }
+}
--- C#1SecondTryHomeWork/Program.cs
+++ C#1SecondTryHomeWork/Program.cs
@@ -4,9 +4,17 @@
     {
         static void Main(string[] args)
         {
-           
-                
-         
+            
+            var a = new Item { Id = 1, Name = "Sword" };
+            var b = new Item { Id = 2, Name = "Shield" };
+
+            Console.WriteLine($"A Hash = {a.GetHashCode()}");
+            Console.WriteLine($"B Hash = {b.GetHashCode()}");
+
+            Console.WriteLine("Equals: " + a.Equals(b));
+            
+
+
         }
     }
 }
